96efad56288a87b82f8761ad64d8b0521fa52dff
zhang-de-lab <50124934+zhang-de-lab@users.noreply.github.com>
2019-05-11 14:00:31 +0800

    Update shili_pre.py

--- osteoporosis/shili_pre.py
+++ osteoporosis/shili_pre.py
@@ -6,10 +6,6 @@ import numpy as np
 from keras.models import model_from_json
 
 def loadmodels():
-	modelG1 = model_from_json(open('models/modelGu.json').read())
-	modelG1.load_weights('models/modelGuGlobal.h5')
-	modelG2 = model_from_json(open('models/modelGu.json').read())
-	modelG2.load_weights('models/modelGuGlobal-1.h5')
 
 	modelC1 = model_from_json(open('models/modelYao.json').read())
 	modelC1.load_weights('models/modelYaoC.h5')
@@ -21,35 +17,14 @@ def loadmodels():
 	modelZ2 = model_from_json(open('models/modelYao.json').read())
 	modelZ2.load_weights('models/modelYaoZ-1.h5')
 
-	return modelG1, modelG2, modelC1, modelC2, modelZ1, modelZ2
+	return modelC1, modelC2, modelZ1, modelZ2
 
 
-# modelG1, modelG2, modelC1, modelC2, modelZ1, modelZ2 = loadmodels()
+# modelC1, modelC2, modelZ1, modelZ2 = loadmodels()
 
 def pre_one(pre_batch, model):
 	out = model.predict_on_batch(pre_batch)
 	return np.mean(out)
-
-def pre_femoral(path):
-	data = sio.loadmat('mat/'+path)
-	x = data['data'].astype('float32')
-	x /= 255. 
-	y = data['label']
-	modelG1, modelG2, _, _, _, _ = loadmodels()
-	num = pre_one(x, modelG1)
-	if num>=0.5:
-		print('预测结果为小于-2.5, 对应标签为0')
-		print('预测结果为小于-2.5的概率为：', num, '  标签为：', y[0][0], '(0代表T值小于-2.5, 1代表T值大于-2.5 小于-1, 2代表T值大于-1)')
-		return
-	if num<0.5:
-		num2 = pre_one(x, modelG2)
-		if num2>=0.5:
-			print('预测结果为大于-1, 对应标签为2')
-		else:
-			print('预测结果为介于-2.5和-1之间, 对应标签为1')
-		print('预测结果为大于-2.5和大于-1的概率分别为：', num, ',' , num2, '  标签为：', y[0][0], '(0代表T值小于-2.5, 1代表T值大于-2.5 小于-1, 2代表T值大于-1)')
-		return 
-
 
 def pre_lumoral(path):
 	data = sio.loadmat('mat/'+path)
@@ -70,7 +45,7 @@ def pre_lumoral(path):
 	x4Z = data['dataL4Z'].astype('float32')
 	x4Z /= 255.
 	y = data['label']
-	_, _, modelC1, modelC2, modelZ1, modelZ2 = loadmodels()
+	modelC1, modelC2, modelZ1, modelZ2 = loadmodels()
 	num11 = pre_one(x1C, modelC1)
 	num12 = pre_one(x2C, modelC1)
 	num13 = pre_one(x3C, modelC1)
@@ -81,8 +56,8 @@ def pre_lumoral(path):
 	num24 = pre_one(x4Z, modelZ1)
 	num = np.mean([num11, num21, num12, num22, num13, num23, num14, num24])
 	if num>=0.5:
-		print('预测结果为小于-2.5, 对应标签为0')
-		print('预测结果为小于-2.5的概率为：', num, '  标签为：', y[0][0], '(0代表T值小于-2.5, 1代表T值大于-2.5 小于-1, 2代表T值大于-1)')
+		print('The predicted results are less than -2.5, The corresponding label is 0')
+		print('The probability that the predicted result is less than -2.5 is ', num, '  label is：', y[0][0], '(0 means that T value is less than - 2.5, 1 means that T value is greater than - 2.5 is less than - 1, and 2 means that T value is greater than - 1)')
 		return
 	if num<0.5:
 		num11 = pre_one(x1C, modelC2)
@@ -95,8 +70,8 @@ def pre_lumoral(path):
 		num24 = pre_one(x4Z, modelZ2)
 		num2 = np.mean([num11, num21, num12, num22, num13, num23, num14, num24])
 		if num2>=0.5:
-			print('预测结果为大于-1, 对应标签为2')
+			print('The predicted result is greater than - 1, and the corresponding label is 2.')
 		else:
-			print('预测结果为介于-2.5和-1之间, 对应标签为1')
-		print('预测结果为大于-2.5和大于-1的概率分别为：', num, ',' , num2, '  标签为：', y[0][0], '(0代表T值小于-2.5, 1代表T值大于-2.5 小于-1, 2代表T值大于-1)')
+			print('The predicted results are between - 2.5 and - 1, and the corresponding label is 1.')
+		print('The probability of the predicted results being greater than - 2.5 and greater than - 1 is respectively ', num, ',' , num2, '  label is', y[0][0], '(0 means that T value is less than - 2.5, 1 means that T value is greater than - 2.5 is less than - 1, and 2 means that T value is greater than - 1)')
 		return
